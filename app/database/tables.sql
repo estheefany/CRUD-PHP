f189a90f890f252cc9bdc698c0d16d1e99200e44
Estheer <104703548+estheefany@users.noreply.github.com>
2023-06-01 22:50:39 -0300

    Update tables.sql

--- app/database/tables.sql
+++ app/database/tables.sql
@@ -6,14 +6,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
     dataNascimento  TEXT,
 );
 
-INSERT INTO usuarios (id, nome, dataNascimento, tipo, ativado) values (1,'teste 1','01-01-2000',1,1);
-INSERT INTO usuarios (id, nome, dataNascimento, tipo, ativado) values (2,'teste 2','01-01-2001',1,1);
-INSERT INTO usuarios (id, nome, dataNascimento, tipo, ativado) values (3,'teste 3','01-01-2003',1,1);
-
-
-/** essa tabela deve ser criada antes de veiculos,
-pois veiculos depende dela
-exemplo de relacionamento UM para MUITOS MODELO x VEICULOS  */
 DROP TABLE IF EXISTS capas;
 CREATE TABLE IF NOT EXISTS capas (
     id              INTEGER PRIMARY KEY,
@@ -23,7 +15,6 @@ CREATE TABLE IF NOT EXISTS capas (
 INSERT INTO capas (id, capa) values (1,'Capa Hardback');
 INSERT INTO capas (id, capa) values (2,'Capa Paperback');
 INSERT INTO capas (id, capa) values (3,'Capa Canoa');
-
 
 
 DROP TABLE IF EXISTS livros;
@@ -38,9 +29,6 @@ CREATE TABLE IF NOT EXISTS livros (
     FOREIGN KEY (capa_id)
     REFERENCES capas (id)
 );
-
-/** essa tabela deve ser criada depois de veiculos e usuarios,
-exemplo de relacionamento MUITOS para MUITOS USUARIOS x VEICULOS */
 
 DROP TABLE IF EXISTS leitores;
 CREATE TABLE IF NOT EXISTS leitores (
